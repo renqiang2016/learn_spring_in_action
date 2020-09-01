08761fffeac7ba9e159773707223fc5e3c4a9957
renqiang <22talentren@sina.com>
2020-09-01 10:27:52 +0800

    new clean project

---
+++ src/main/java/com/ren/learn_spring_in_action/LearnSpringInActionApplication.java
@@ -0,0 +1,13 @@
+package com.ren.learn_spring_in_action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringInActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringInActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ren/learn_spring_in_action/LearnSpringInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ren.learn_spring_in_action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringInActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
